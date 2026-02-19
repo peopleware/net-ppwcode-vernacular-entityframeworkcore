49ccd824121e2095f41f5de707f0dce49fd58685
Danny Van den Wouwer <dvdwouwe@peopleware.be>
2026-02-19 16:31:20 +0100

    Add documentation

--- src/I/Interceptors/AuditableInterceptor.cs
+++ src/I/Interceptors/AuditableInterceptor.cs
@@ -19,16 +19,35 @@ using PPWCode.Vernacular.RequestContext.I;
 
 namespace PPWCode.Vernacular.EntityFrameworkCore.I.Interceptors;
 
+/// <summary>
+///     Provides an entity framework interceptor to automatically populate auditing metadata on entities
+///     implementing <see cref="IInsertAuditable{TTimestamp}" /> and <see cref="IUpdateAuditable{TTimestamp}" />.
+/// </summary>
+/// <typeparam name="TTimestamp">
+///     The value type used for timestamps, typically <see cref="DateTime" /> or <see cref="DateTimeOffset" />.
+/// </typeparam>
 public abstract class AuditableInterceptor<TTimestamp> : SaveChangesInterceptor
     where TTimestamp : struct, IComparable<TTimestamp>, IEquatable<TTimestamp>
 {
     private readonly IRequestContext<TTimestamp> _requestContext;
 
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="AuditableInterceptor{TTimestamp}" /> class.
+    /// </summary>
+    /// <param name="requestContext">The context providing current request metadata such as identity and timestamp.</param>
     protected AuditableInterceptor(IRequestContext<TTimestamp> requestContext)
     {
         _requestContext = requestContext;
     }
 
+    /// <summary>
+    ///     Intercepts the asynchronous saving process to apply auditing logic before data is committed to the database.
+    /// </summary>
+    /// <param name="eventData">Contextual data regarding the DbContext event.</param>
+    /// <param name="result">The current interception result.</param>
+    /// <param name="cancellationToken">A token to observe while waiting for the task to complete.</param>
+    /// <returns>A <see cref="ValueTask" /> representing the asynchronous operation.</returns>
+    /// <exception cref="ProgrammingError">Thrown when the <see cref="DbContext" /> is null.</exception>
     public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
         DbContextEventData eventData,
         InterceptionResult<int> result,
@@ -46,17 +65,31 @@ public abstract class AuditableInterceptor<TTimestamp> : SaveChangesInterceptor
         return base.SavingChangesAsync(eventData, result, cancellationToken);
     }
 
+    /// <inheritdoc />
+    /// <remarks>
+    ///     This synchronous implementation wraps the asynchronous call.
+    ///     Note: It is recommended to use asynchronous saving in modern applications.
+    /// </remarks>
     public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
         => SavingChangesAsync(eventData, result)
             .ConfigureAwait(false)
             .GetAwaiter()
             .GetResult();
 
+    /// <summary>
+    ///     Iterates through the <see cref="ChangeTracker" /> to update auditing properties based on the state of the entities.
+    /// </summary>
+    /// <param name="context">The current <see cref="DbContext" /> instance.</param>
+    /// <remarks>
+    ///     Entities in the <see cref="EntityState.Added" /> state get their creation metadata set.
+    ///     Entities in the <see cref="EntityState.Modified" /> state get their last modification metadata updated.
+    /// </remarks>
     protected virtual void UpdateAuditableEntities(DbContext context)
     {
         object requestTimestamp = _requestContext.RequestTimestamp;
         object identityName = _requestContext.IdentityName;
 
+        // Populate creation audit fields for new entities
         foreach (EntityEntry<IInsertAuditable<TTimestamp>> entityEntry in
                  context
                      .ChangeTracker
@@ -67,6 +100,7 @@ public abstract class AuditableInterceptor<TTimestamp> : SaveChangesInterceptor
             entityEntry.Property(nameof(IInsertAuditable<TTimestamp>.CreatedBy)).CurrentValue = identityName;
         }
 
+        // Populate modification audit fields for updated entities
         foreach (EntityEntry<IUpdateAuditable<TTimestamp>> entityEntry in
                  context
                      .ChangeTracker
--- src/I/Interceptors/WildExceptionsInterceptor.cs
+++ src/I/Interceptors/WildExceptionsInterceptor.cs
@@ -10,6 +10,7 @@
 // limitations under the License.
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 
 using PPWCode.Vernacular.Exceptions.V;
@@ -17,8 +18,21 @@ using PPWCode.Vernacular.Semantics.V;
 
 namespace PPWCode.Vernacular.EntityFrameworkCore.I.Interceptors;
 
+/// <summary>
+///     An Entity Framework interceptor that performs domain-level validation before any changes are saved to the database.
+///     It ensures that all entities implementing <see cref="ICivilizedObject" /> pass their internal validation rules.
+/// </summary>
 public class WildExceptionsInterceptor : SaveChangesInterceptor
 {
+    /// <summary>
+    ///     Intercepts the asynchronous save process to execute domain validation.
+    /// </summary>
+    /// <param name="eventData">Contextual information about the current <see cref="DbContext" /> operation.</param>
+    /// <param name="result">The current result of the interception.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>A task representing the asynchronous operation, containing the interception result.</returns>
+    /// <exception cref="CompoundSemanticException">Thrown when one or more entities fail their validation checks.</exception>
+    /// <exception cref="ProgrammingError">Thrown if the <see cref="DbContext" /> is not properly initialized.</exception>
     public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
         DbContextEventData eventData,
         InterceptionResult<int> result,
@@ -40,16 +54,33 @@ public class WildExceptionsInterceptor : SaveChangesInterceptor
         return base.SavingChangesAsync(eventData, result, cancellationToken);
     }
 
+    /// <inheritdoc />
+    /// <remarks>
+    ///     This method synchronously executes the asynchronous validation logic.
+    ///     Usage of <c>SaveChangesAsync</c> is generally preferred in modern applications.
+    /// </remarks>
     public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
         => SavingChangesAsync(eventData, result)
             .ConfigureAwait(false)
             .GetAwaiter()
             .GetResult();
 
+    /// <summary>
+    ///     Scans the <see cref="ChangeTracker" /> for added or modified entities that implement
+    ///     <see cref="ICivilizedObject" />
+    ///     and aggregates any validation errors they report.
+    /// </summary>
+    /// <param name="dbContext">The current database context containing the tracked changes.</param>
+    /// <returns>A <see cref="CompoundSemanticException" /> containing all gathered validation errors.</returns>
+    /// <remarks>
+    ///     Only entities in the <see cref="EntityState.Added" /> or <see cref="EntityState.Modified" /> states are validated,
+    ///     as these represent the data currently being pushed to the store.
+    /// </remarks>
     private CompoundSemanticException Validate(DbContext dbContext)
     {
         CompoundSemanticException cse = new();
 
+        // Identify objects that require domain-rule checking
         IEnumerable<ICivilizedObject> civilizedObjects =
             dbContext
                 .ChangeTracker
@@ -57,8 +88,10 @@ public class WildExceptionsInterceptor : SaveChangesInterceptor
                 .Where(e => e.State is EntityState.Added or EntityState.Modified)
                 .Select(e => e.Entity)
                 .OfType<ICivilizedObject>();
+
         foreach (ICivilizedObject civilizedObject in civilizedObjects)
         {
+            // Gather "wild" exceptions from each object and add them to the compound exception
             cse.AddElement(civilizedObject.WildExceptions());
         }
 
